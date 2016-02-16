a221fa22f2b20602b60a114718d932eb0f2acf4b
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-02-16 14:45:14 +0100

    [MIG] Use new API

--- cmis/unit/backend_adapter.py
+++ cmis/unit/backend_adapter.py
@@ -2,42 +2,33 @@
 # © 2014-2015 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.osv import orm
+from cmislib.model import CmisClient
+import cmislib.exceptions
+import urllib2
+
 from openerp.tools.translate import _
 from openerp.addons.connector.unit.backend_adapter import CRUDAdapter
-import cmislib.exceptions
-from cmislib.model import CmisClient
-import urllib2
+from openerp.addons.cmis.exceptions import CMISConnectionError
 
 
 class CmisAdapter(CRUDAdapter):
 
-    def _auth(self, ids):
+    def _auth(self, cmis_backend):
         """Test connection with CMIS"""
-        if type(ids) is not list:
-            ids = [ids]
-        # Get the url, user and password for CMIS
-        # ids = self.search(cr, uid, [])
-        if not ids:
-            raise orm.except_orm(
-                _('Internal Error'),
-                _('Something very wrong happened. _auth() '
-                    'called without any ids.')
-            )
-        res = self.backend_record.read(['location', 'username', 'password'])[0]
-        url = res['location']
-        user_name = res['username']
-        user_password = res['password']
-        client = CmisClient(url, user_name, user_password)
+        cmis_backend.ensure_one()
+        client = CmisClient(
+            cmis_backend.location,
+            cmis_backend.username, 
+            cmis_backend.password)
 
         try:
             return client.defaultRepository
         except cmislib.exceptions.ObjectNotFoundException:
-            raise orm.except_orm(_('CMIS connection Error!'),
-                                 _("Check your CMIS account configuration."))
+            raise CMISConnectionError(
+                _("Check your CMIS account configuration."))
         except cmislib.exceptions.PermissionDeniedException:
-            raise orm.except_orm(_('CMIS connection Error!'),
-                                 _("Check your CMIS account configuration."))
+            raise CMISConnectionError(
+                _("Check your CMIS account configuration."))
         except urllib2.URLError:
-            raise orm.except_orm(_('CMIS connection Error!'),
-                                 _("SERVER is down."))
+            raise CMISConnectionError(
+                _("SERVER is down."))
