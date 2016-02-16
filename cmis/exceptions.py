ec23c43b4b7854edd91f04f8dbac89fb2325013d
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-02-16 14:50:40 +0100

    [MIG] Move Odoo models to module 'models'

--- cmis/exceptions.py
+++ cmis/exceptions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2014-2015 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# © 2016 ACSONE SA/NV (<http://acsone.eu>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.exceptions import UserError
