8f5aa050227277e090b035dbf105e28da5335c41
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-02-17 13:44:16 +0100

    [MIG]Fix import after move

---
+++ cmis_write/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# © 2014-2015 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import ir_attachment_doc_backend
+from . import ir_attachment_download
+from . import ir_attachment
+from . import metadata_list
+from . import metadata
