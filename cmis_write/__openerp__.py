1d44535fb7daec4fe7e1371e46b7cf180cf56f2d
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-02-17 14:58:28 +0100

    [MIG] move description to README.rst

--- cmis_write/__openerp__.py
+++ cmis_write/__openerp__.py
@@ -7,48 +7,8 @@
     'version': '9.0.1.0.0',
     'category': 'Knowledge Management',
     'summary': 'Create Document in DMS from Odoo/OpenERP',
-    'description': """
-Add Documents from Odoo/OpenERP
-===============================
-
-This module allows you to store Odoo/OpenERP document in the DMS repository.
-
-Configuration
-=============
-
-* Create a new CMIS backend with the host, login and password.
-* Configure the path in the repository where documents will be dropped.
-  By default, it uses the home directory of the user.
-
-Usage
-=====
-
-* On one Odoo/OpenERP record, click "Add document".
-* Upload your documents
-* Uploaded documents will be enqueued for storage in the DMS
-  (Document Management System)
-
-Add Metadata
-============
-
-To manage a custom aspect using CMIS (and all the other supported ways)
-you have to:
-
-* Define a new custom model configuring Alfresco. To do this I suggest you
-http://wiki.alfresco.com/wiki/Step-By-Step:_Creating_A_Custom_Model.
-
-* Add the custom aspect to the document you upload or create in Alfresco.
-Using CMIS I suggest you:
-http://docs.alfresco.com/4.1/index.jsp?topic=%2Fcom.alfresco.enterprise.doc%2Fconcepts%2Fopencmis-ext-adding.html.
-
-* Set the custom property in the way you probably know using CMIS.
-
-Contributors
-------------
-* El Hadji Dem (elhadji.dem@savoirfairelinux.com)
-""",
-    'author': 'Savoir-faire Linux',
-    'website': 'www.savoirfairelinux.com',
+    'author': 'Savoir-faire Linux, Odoo Community Association (OCA)',
+    'website': 'https://odoo-community.org/',
     'license': 'AGPL-3',
     'depends': [
         'document',
