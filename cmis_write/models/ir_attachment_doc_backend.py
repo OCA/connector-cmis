6931a6ccc81b8cb493c10cd6ea007f4676927164
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-02-17 18:01:20 +0100

    [MIG] Use new API

--- cmis_write/models/ir_attachment_doc_backend.py
+++ cmis_write/models/ir_attachment_doc_backend.py
@@ -2,28 +2,26 @@
 # © 2014-2015 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.osv import orm, fields
+from openerp import fields, models
 
 
-class ir_attachment_doc_backend(orm.Model):
+class IrAttachmentDocBackend(models.Model):
     _description = "List of backend's document"
     _name = 'ir.attachment.doc.backend'
 
-    _columns = {
-        'attachment_id': fields.many2one(
-            'ir.model.fields',
-            'Document',
-            ondelete='cascade',
-            help="Document"
-        ),
-        'backend_id': fields.many2one(
-            'cmis.backend',
-            'Backend',
-            ondelete='cascade',
-            help="Backend"
-        ),
-        'object_doc_id': fields.char(
-            "Id of document backend",
-            help="Id of document backend"
-        ),
-    }
+    attachment_id = fields.Many2one(
+        'ir.model.fields',
+        'Document',
+        ondelete='cascade',
+        help="Document"
+    )
+    backend_id = fields.Many2one(
+        'cmis.backend',
+        'Backend',
+        ondelete='cascade',
+        help="Backend"
+    )
+    object_doc_id = fields.Char(
+        "Id of document backend",
+        help="Id of document backend"
+    )
