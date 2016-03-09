68c1240eb7358571b5f8128e46830232c6c8b709
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-03-09 12:58:34 +0100

    [FIX] Disable SSL certificat validation

--- cmis/__init__.py
+++ cmis/__init__.py
@@ -2,6 +2,14 @@
 # © 2014-2015 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import httplib2
+import functools
+logger = logging.getLogger(__name__)
+
+logger.warning('Disable SSL Certificate Validation by python code')
+httplib2.Http = functools.partial(
+    httplib2.Http, disable_ssl_certificate_validation=True)
+
 from . import (
     backend,
     models,
