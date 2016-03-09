b477fb6b71b92f74c031e311c78ef3b83ea68120
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-03-09 13:05:31 +0100

    FIX

--- cmis/__init__.py
+++ cmis/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2014-2015 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+import logging
 import httplib2
 import functools
 logger = logging.getLogger(__name__)
