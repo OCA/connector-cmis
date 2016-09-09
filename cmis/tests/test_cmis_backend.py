771b961cd757af22b0ebf70daca7c3505372e192
Laurent Mignon <laurent.mignon@acsone.eu>
2016-09-09 16:39:31 +0200

    Migrate CMIS from 7.0 It no longer depends on the connector module. The goal is to have a base module to define the configuration for accessing a CMIS repository and providing a 'client' to communicate with this repository. This module can thus be the basis for different integration scenarios with a CMIS container. (Via the connector module or via specialized fields etc)

---
+++ cmis/tests/test_cmis_backend.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# © 2014-2015 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+# Copyright 2016 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests import common
+
+
+class TestCmisBackend(common.SavepointCase):
+
+    def setUp(self):
+        super(TestCmisBackend, self).setUp()
+        self.vals = {
+            'name': "Test cmis",
+            'location': "http://localhost:8081/alfresco/s/cmis",
+            'username': 'admin',
+            'password': 'admin',
+            'initial_directory_write': '/',
+        }
+        self.cmis_backend = self.env['cmis.backend']
+        self.backend_instance = self.cmis_backend.create(
+            self.vals)
+
+    def test_get_by_name(self):
+        backend = self.cmis_backend.get_by_name(name=self.vals['name'])
+        self.assertEquals(self.backend_instance, backend)
+        with self.assertRaises(ValueError):
+            self.cmis_backend.get_by_name('error')
+
+    def test_clear_caches(self):
+        backend = self.cmis_backend.get_by_name(name=self.vals['name'])
+        self.assertEquals(self.backend_instance, backend)
+        backend.write({'name': 'new name'})
+        backend = self.cmis_backend.get_by_name(name='new name')
+        self.assertEquals(backend.name, 'new name')
