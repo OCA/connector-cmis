8657eee0a4791fea2ef8fa36104659b3e8608223
Laurent Mignon <laurent.mignon@acsone.eu>
2016-09-09 16:39:31 +0200

    Migrate CMIS from 7.0 It no longer depends on the connector module. The goal is to have a base module to define the configuration for accessing a CMIS repository and providing a 'client' to communicate with this repository. This module can thus be the basis for different integration scenarios with a CMIS container. (Via the connector module or via specialized fields etc)

--- cmis/models/__init__.py
+++ cmis/models/__init__.py
@@ -2,5 +2,4 @@
 # © 2014-2015 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import test_cmis_backend
-
+from . import cmis_backend
