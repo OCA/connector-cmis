0946d32d605b76a5405bb7b43fb64fd336ff7583
Laurent Mignon <laurent.mignon@acsone.eu>
2016-09-09 17:01:14 +0200

    Fix docstring and remove ssl monkey patch

--- cmis/__init__.py
+++ cmis/__init__.py
@@ -1,13 +1,4 @@
 # -*- coding: utf-8 -*-
 # © 2014-2015 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-import logging
-import httplib2
-import functools
 from . import models
-
-logger = logging.getLogger(__name__)
-
-logger.warning('Disable SSL Certificate Validation by python code')
-httplib2.Http = functools.partial(
-    httplib2.Http, disable_ssl_certificate_validation=True)
