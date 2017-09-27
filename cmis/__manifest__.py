bc800b4fddc79ca90747822c81479974e9551aff
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2017-09-27 09:52:08 -0500

    [FIX] Migration stuff and menuitem

--- cmis/__manifest__.py
+++ cmis/__manifest__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 # © 2014-2015 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -18,7 +17,6 @@
     },
     'data': [
         'security/cmis_backend.xml',
-        'views/cmis_menu.xml',
         'views/cmis_backend.xml',
     ],
     'demo': [
