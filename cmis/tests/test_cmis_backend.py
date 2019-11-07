9af8e316aa332fe039a209d3146f9b2167a9f4ac
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-07 11:37:12 +0100

    [MIG] cmis: black, isort

--- cmis/tests/test_cmis_backend.py
+++ cmis/tests/test_cmis_backend.py
@@ -3,24 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from psycopg2._psycopg import IntegrityError
+
 from odoo.tests import common
 from odoo.tools import mute_logger
 
 
 class TestCmisBackend(common.SavepointCase):
-
     def setUp(self):
         super(TestCmisBackend, self).setUp()
         self.vals = {
-            'name': "Test cmis",
-            'location': "http://localhost:8081/alfresco/s/cmis",
-            'username': 'admin',
-            'password': 'admin',
-            'initial_directory_write': '/',
+            "name": "Test cmis",
+            "location": "http://localhost:8081/alfresco/s/cmis",
+            "username": "admin",
+            "password": "admin",
+            "initial_directory_write": "/",
         }
-        self.cmis_backend = self.env['cmis.backend']
-        self.backend_instance = self.cmis_backend.create(
-            self.vals)
+        self.cmis_backend = self.env["cmis.backend"]
+        self.backend_instance = self.cmis_backend.create(self.vals)
 
     @mute_logger("odoo.sql_db")
     def test_unique_name(self):
