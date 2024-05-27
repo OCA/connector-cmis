b4f2dbaa325a3bfde79932b3b06cdb90fdd6293e
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2024-05-27 15:50:55 +0200

    [FIX] cmis: CMIS Backend form view

--- cmis/__manifest__.py
+++ cmis/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "CMIS",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "development_status": "Mature",
     "category": "Connector",
     "summary": "Connect Odoo with a CMIS server",
